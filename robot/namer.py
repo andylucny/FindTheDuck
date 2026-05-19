c826fe43cdb9735fa5bc921438dcfd9600b6c02e
Linus Szillat <linus.szillat100@web.de>
2026-05-19 17:19:15 +0200

    Set flush=True

--- robot/namer.py
+++ robot/namer.py
@@ -26,6 +26,6 @@ class Namer(Agent):
         features = space[self.name]
         if features is not None:
             similarity = features @ self.duck
-            print(similarity)
+            print(similarity, flush=True)
             if similarity > self.threshold:
                 space['tospeak'] = "Ou, here is the duck!"
